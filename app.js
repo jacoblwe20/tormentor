b5e22ed78f759962e0b06525c1f25ddeb087ded8
jacoblwe20 <jacoblowe2.0@gmail.com>
2013-03-12 23:45:39 -0700

    adding a simple gui to manage remote tabs

--- app.js
+++ app.js
@@ -8,9 +8,21 @@ var express = require('express')
   , path = require('path');
 
 var app = express();
-var sockets = [];
+var sockets = {};
 var admins = [];
 var clients = {};
+var close = function(data){
+  // if(data.closeall){
+  //   for(var i = 0; i < sockets.length; i +=1){
+  //     var socket = sockets[i];
+  //     socket.emit("close", {});
+  //   }
+  // }
+  if(data.id && data.tab){
+    console.log(["sending signal", data])
+    sockets[data.id].emit("close", {id : data.tab});
+  }
+};
 
 app.locals.title = "Websocket Server";
 
@@ -38,21 +50,6 @@ app.get("/test", function(req, res){
   res.render("test");
 });
 
-app.post("/", function(req, res){
-  var data = req.body;
-  console.log(data);
-  if(data.closeall){
-    for(var i = 0; i < sockets.length; i +=1){
-      var socket = sockets[i];
-      socket.emit("close", {});
-    }
-  }
-  if(data.id && data.tab){
-    clients[data.id].socket.emit("close", {id : data.tab});
-  }
-  res.render("index");
-});
-
 var app_ = http.createServer(app);
 
 app_.listen(app.get('port'), function(){
@@ -61,7 +58,7 @@ app_.listen(app.get('port'), function(){
 
 var io = require("socket.io").listen(app_);
 var data = function(msg, data){
-  for(var i = 0; i < admins.length; i +=1){
+  for(var i = 0; i < admins.length; i += 1){
     var socket = admins[i];
     socket.emit(msg, data);
   }
@@ -70,32 +67,47 @@ var data = function(msg, data){
 io.sockets.on("connection", function(socket){
   //console.log(socket);
   var client = (Math.random() * 2342342342).toString().replace(/\./, "O");
-  sockets.push(socket);
-  clients[client] = {socket : socket};
   // either need to send out client id to client or retrieve the client id from the client so that we can have persistant data
-  data("new_client", {client : client});
+  socket.on("new_client", function(message){
+    var client = message.client_id;
+
+    clients[client] = {tabs : message.tabs};
+    sockets[client] = socket;
+    data("new_client", message);
+
+
+    socket.on("new_tab", function(message){
+      if(!clients[client].tabs) clients[client].tabs = {};
+      clients[client].tabs[message.tab.id] = message.tab;
+      data("new_tab", {client : client, message : message});
+      //console.log({client : client, tab: message.tab});
+    });
+
+    socket.on("close_tab", function(message){
+      if(!clients[client].tabs) clients[client].tabs = {};
+      delete clients[client].tabs[message.tabid];
+      data("close_tab", {client : client, message : message});
+    });
+
+    socket.on("url_change", function(message){
+      clients[client].tabs[message.tabid].url = message.url;
+      data("url_change", {client : client, message : message});
+    });
+
+    socket.on('disconnect', function () {
+      delete clients[client];
+      data("disconnect", {client : client});
+    });
+
+  });
 
   socket.on("admin", function(){
     admins.push(socket);
-    socket.emit("init", clients);
-  });
-
-  socket.on("new_tab", function(message){
-    if(!clients[client].tabs) clients[client].tabs = {};
-    clients[client].tabs[message.tab.id] = message.tab;
-    data("new_tab", {client : client, message : message});
-    console.log({client : client, tab: message.tab});
-  });
-
-  socket.on("close_tab", function(message){
-    if(!clients[client].tabs) clients[client].tabs = {};
-    delete clients[client].tabs[message.tabid];
-    data("close_tab", {client : client, message : message});
-  });
-
-  socket.on('disconnect', function () {
-    delete clients[client];
-    data("disconnect", {client : client});
+    socket.emit("init", {clients : clients});
+    socket.on("close", function(data){
+      close(data);
+      //console.log("closing");
+    });
   });
 
 });
---
+++ public/javascripts/app.js
@@ -0,0 +1,104 @@
+(function(exports){
+
+	var clients;
+	var content = $(".content");
+	var Cookie = function(){
+		var cookie = decodeURIComponent(document.cookie);
+		var obj = {};
+		cookie = cookie.split(/\=/g);
+		obj[cookie[0]] = cookie[1];
+		return obj;
+	}();
+	var view = "<ul>"+
+		"{{#clients}}" +
+		"<li data-id={{id}} >" +
+			"{{id}}" +
+			"<ul>" +
+			"{{#tabs}}" +
+			"<li data-id={{id}}>" +
+				"{{url}} &nbsp;&nbsp;&nbsp;&nbsp;" +
+				"<a href=# class=close >Close</a>" + 
+			"</li>" +
+			"{{/tabs}}" + 
+			"</ul>" +
+		"{{/clients}}" +
+	"</ul>";
+	var reformat = function(){
+		var clis = [];
+		for(var key in clients){
+			var client = clients[key];
+			var tabs = [];
+			if(client.tabs){
+				for(var index in client.tabs){
+					tabs.push({id : index, url : client.tabs[index].url});
+				}
+			}
+			clis.push({id : key, tabs : tabs});
+		}
+		return clis;
+	};
+	var refresh = function(){
+		var data = {clients : reformat()};
+		content.html(Mustache.render(view, data));
+	};
+	var socket = io.connect(Cookie.uri);
+
+	content.on("click", ".close", function(){
+		var parent = $(this).parent("li");
+		var obj = {
+			tab : parent.data("id"),
+			id : parent.parent("ul").parent("li").data("id")
+		};
+
+		socket.emit("close", obj);
+	});
+
+	socket.emit("admin", {admin : true});
+
+	socket.on('close', function (data) {
+	   console.log(data);
+	});
+
+	socket.on("init", function(data){
+		clients = data.clients;
+		refresh();
+	});
+
+	socket.on("new_tab", function(data){
+		if(!clients[data.client].tabs) clients[data.client].tabs = {};
+		clients[data.client].tabs[data.message.tab.id] = data.message.tab;
+		refresh();
+	});
+
+	socket.on("close_tab", function(data){
+		//onsole.log(data);
+		if(!clients[data.client].tabs) clients[data.client].tabs = {};
+		delete clients[data.client].tabs[data.message.tabid];
+		refresh();
+	});
+
+	socket.on("new_client", function(data){
+		//console.log(data);
+		clients[data.client_id] = {tabs : data.tabs};
+		refresh();
+	});
+
+	socket.on("url_change", function(data){
+		//console.log(data);
+		if(clients[data.client].tabs[data.message.tabid]){
+			clients[data.client].tabs[data.message.tabid].url = data.message.url;
+			refresh();
+		}
+	});
+
+	socket.on("disconnect", function(data){
+		//console.log(data);
+		delete clients[data.client];
+		refresh();
+	});
+
+	exports.views = view;
+	exports.clients = clients;
+
+
+}(this))
