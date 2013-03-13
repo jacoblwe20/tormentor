8f5abd47ae860a9273724a54f9fbee699fc22752
jacoblwe20 <jacoblowe2.0@gmail.com>
2013-03-13 00:05:06 -0700

    setting up test url to register on client list

--- public/javascripts/test.js
+++ public/javascripts/test.js
@@ -1,5 +1,7 @@
-var socket = io.connect('http://sudo.servebeer.com:8080');
+var socket = io.connect('http://127.0.0.1:3000');
 
 socket.on('close', function (data) {
    console.log(data);
 });
+var client = Math.random() * 100 + "tester";
+socket.emit("new_client", {client_id : client, tabs : []});
